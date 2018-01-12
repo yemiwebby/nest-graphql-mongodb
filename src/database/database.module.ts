b992a4795da532a4b7f84de08dea8bff48c847d7
Clayton Ray <iamclaytonray@gmail.com>
2018-01-12 13:26:43 -0500

    feat(database): Add database module

---
+++ src/database/database.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest-graphql-mongodb'),
+  ]
+})
+export class DatabaseModule {}
