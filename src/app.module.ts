5b03c4dfe638b880cd7ba4d661078c42259364cb
Clayton Ray <iamclaytonray@gmail.com>
2018-01-12 14:34:52 -0500

    fix: Add PostModule to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { GQLModule } from './graphql/gql.module';
+import { PostModule } from './posts/post.module';
 import { UserModule } from './users/user.module';
 
 @Module({
-  imports: [ DatabaseModule, GQLModule, UserModule ],
+  imports: [ DatabaseModule, GQLModule, UserModule, PostModule ],
 })
 export class ApplicationModule {}
