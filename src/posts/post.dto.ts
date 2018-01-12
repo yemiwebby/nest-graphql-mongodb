21e339caee91492bb9a36ea6735fdd6c57217441
Clayton Ray <iamclaytonray@gmail.com>
2018-01-12 14:35:39 -0500

    chore(types): Update post interface & dto to have optional fields

--- src/posts/post.dto.ts
+++ src/posts/post.dto.ts
@@ -1,10 +1,10 @@
 import { IUser } from '../users/user.interface';
 
 export class CreatePostDto {
-  public readonly author: IUser;
-  public readonly createdAt: Date;
+  public readonly author?: IUser;
+  public readonly createdAt?: Date;
   public readonly id: string;
   public readonly title: string;
   public readonly slug: string;
-  public readonly updatedAt: Date;
+  public readonly updatedAt?: Date;
 }
--- src/posts/post.interface.ts
+++ src/posts/post.interface.ts
@@ -2,10 +2,10 @@ import { Document } from 'mongoose';
 import { IUser } from '../users/user.interface';
 
 export interface IPost extends Document {
-  readonly author: IUser;
-  readonly createdAt: Date;
+  readonly author?: IUser;
+  readonly createdAt?: Date;
   readonly id: string;
   readonly title: string;
   readonly slug: string;
-  readonly updatedAt: Date;
+  readonly updatedAt?: Date;
 }
